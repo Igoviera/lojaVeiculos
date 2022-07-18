f15c7b09e532ae322d75fead16a4eaf43cabe457
Igo vieira <igo.vieira@live.com>
2022-07-18 20:41:41 -0300

    Alteração na url da imagem

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -36,7 +36,7 @@ const {deletarVeiculo}:any = useContext(CartContext);
           </div>
 
         <div>
-          <img className={styles.imgCarro} alt={nome} src={image}/>
+          <img className={styles.imgCarro} alt={nome} src={`https://app-loja-api.herokuapp.com/${image}`}/>
           <p>{marca} {nome}</p>
           <p>Preço: {valor} </p>
           <p>Descrição: {desc}</p>
